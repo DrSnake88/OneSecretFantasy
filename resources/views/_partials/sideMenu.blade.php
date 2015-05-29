ce958911afe9cbfa88671010a0b89eba2b778188
DrSnake88 <raynoldvanheyningen@gmail.com>
2015-05-29 15:53:21 +0200

    Full menu

--- resources/views/_partials/sideMenu.blade.php
+++ resources/views/_partials/sideMenu.blade.php
@@ -2,29 +2,29 @@
 <div class="offcanvas-nav">
     <!-- Head (Fixed Part) -->
     <div class="nav-head">
-        {{--<div class="top-bar">--}}
-            {{--<form class="search-box">--}}
-                {{--<span class="search-toggle waves-effect waves-light"></span>--}}
-                {{--<input type="text" id="search-field">--}}
-                {{--<button type="submit" class="search-btn"><i class="flaticon-search100"></i></button>--}}
-            {{--</form>--}}
-            {{--<div class="nav-close waves-effect waves-light waves-circle" data-offcanvas="close"><i class="flaticon-close47"></i></div>--}}
-            {{--<div class="social-buttons">--}}
-                {{--<a href="#" class="sb-twitter"><i class="bi-twitter"></i></a>--}}
-                {{--<a href="#" class="sb-google-plus"><i class="bi-gplus"></i></a>--}}
-                {{--<a href="#" class="sb-facebook"><i class="bi-facebook"></i></a>--}}
-            {{--</div>--}}
-        {{--</div>--}}
-        {{--<a href="index.html" class="offcanvas-logo">--}}
-            {{--<div class="icon"><img src="/img/logo-big-ns.png" alt="One Secret Fantasy"></div>--}}
-            {{--<div class="title">--}}
-                {{--One Secret Fantasy--}}
-                {{--<span>Life your dream!</span>--}}
-            {{--</div>--}}
-        {{--</a>--}}
+        <div class="top-bar">
+            <form class="search-box">
+                <span class="search-toggle waves-effect waves-light"></span>
+                <input type="text" id="search-field">
+                <button type="submit" class="search-btn"><i class="flaticon-search100"></i></button>
+            </form>
+            <div class="nav-close waves-effect waves-light waves-circle" data-offcanvas="close"><i class="flaticon-close47"></i></div>
+            <div class="social-buttons">
+                <a href="#" class="sb-twitter"><i class="bi-twitter"></i></a>
+                <a href="#" class="sb-google-plus"><i class="bi-gplus"></i></a>
+                <a href="#" class="sb-facebook"><i class="bi-facebook"></i></a>
+            </div>
+        </div>
+        <a href="index.html" class="offcanvas-logo">
+            <div class="icon"><img src="/img/logo-big-ns.png" alt="One Secret Fantasy"></div>
+            <div class="title">
+                One Secret Fantasy
+                <span>Life your dream!</span>
+            </div>
+        </a>
         <a href="#" data-toggle="modal" data-target="#signin-page" data-modal-form="sign-in" class="light-color nav-link">Sign In</a>
         <a href="#" data-toggle="modal" data-target="#signin-page" data-modal-form="sign-up" class="light-color nav-link">Sign Up</a>
-        {{--<a href="#" class="btn btn-flat btn-light icon-left waves-effect waves-light"><i class="flaticon-download164"></i> Download</a>--}}
+        <a href="#" class="btn btn-flat btn-light icon-left waves-effect waves-light"><i class="flaticon-download164"></i> Download</a>
     </div>
     <!-- Body (Scroll Part) -->
     <div class="nav-body">
@@ -44,63 +44,63 @@
                 </nav>
 
 
-                {{--<!-- Twitter/Blog Tabs -->--}}
-                {{--<div class="offcanvas-tabs">--}}
-                    {{--<ul class="nav-tabs clearfix">--}}
-                        {{--<li class="active"><a class="waves-effect waves-primary" href="#twitter" data-toggle="tab">Twitter</a></li>--}}
-                        {{--<li><a class="waves-effect waves-primary" href="#blog" data-toggle="tab">Blog</a></li>--}}
-                    {{--</ul>--}}
-                    {{--<div class="tab-content">--}}
-                        {{--<div class="tab-pane fade in active" id="twitter">--}}
-                            {{--<div class="twitter-feed">--}}
-                                {{--<div class="tweet">--}}
-                                    {{--<a href="#" class="author">@@OneSecretFantasy</a>--}}
-                                    {{--<p>--}}
-                                        {{--Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor et <a href="#">#magna aliqua</a>.--}}
-                                    {{--</p>--}}
-                                {{--</div>--}}
-                                {{--<div class="tweet">--}}
-                                    {{--<a href="#" class="author">@@OneSecretFantasy</a>--}}
-                                    {{--<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>--}}
-                                {{--</div>--}}
-                                {{--<a href="#" class="text-smaller">Follow us on twitter</a>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                        {{--<div class="tab-pane fade" id="blog">--}}
-                            {{--<div class="offcanvas-posts">--}}
-                                {{--<a href="/Blog/Post" class="post">--}}
-                                    {{--Blog Title--}}
-                                    {{--<span>Lorem ipsum dolor sit</span>--}}
-                                {{--</a>--}}
-                                {{--<a href="/Blog/Post" class="post">--}}
-                                    {{--Blog Title--}}
-                                    {{--<span>Tenetur omnis sit odit velit quaerat deserunt cupiditate.</span>--}}
-                                {{--</a>--}}
-                                {{--<a href="/Blog/Post" class="post">--}}
-                                    {{--Blog Title--}}
-                                    {{--<span>Sed ut perspiciatis unde omnis</span>--}}
-                                {{--</a>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
+                <!-- Twitter/Blog Tabs -->
+                <div class="offcanvas-tabs">
+                    <ul class="nav-tabs clearfix">
+                        <li class="active"><a class="waves-effect waves-primary" href="#twitter" data-toggle="tab">Twitter</a></li>
+                        <li><a class="waves-effect waves-primary" href="#blog" data-toggle="tab">Blog</a></li>
+                    </ul>
+                    <div class="tab-content">
+                        <div class="tab-pane fade in active" id="twitter">
+                            <div class="twitter-feed">
+                                <div class="tweet">
+                                    <a href="#" class="author">@@OneSecretFantasy</a>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor et <a href="#">#magna aliqua</a>.
+                                    </p>
+                                </div>
+                                <div class="tweet">
+                                    <a href="#" class="author">@@OneSecretFantasy</a>
+                                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.</p>
+                                </div>
+                                <a href="#" class="text-smaller">Follow us on twitter</a>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade" id="blog">
+                            <div class="offcanvas-posts">
+                                <a href="/Blog/Post" class="post">
+                                    Blog Title
+                                    <span>Lorem ipsum dolor sit</span>
+                                </a>
+                                <a href="/Blog/Post" class="post">
+                                    Blog Title
+                                    <span>Tenetur omnis sit odit velit quaerat deserunt cupiditate.</span>
+                                </a>
+                                <a href="/Blog/Post" class="post">
+                                    Blog Title
+                                    <span>Sed ut perspiciatis unde omnis</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
 
 
-                {{--<!-- Instagram Posts -->--}}
-                {{--<div class="offcanvas-instagram">--}}
-                    {{--<div class="instgr-row clearfix">--}}
-                        {{--<a class="waves-effect" href="#"><img src="/img/people/man1-small.jpg" alt="Thumbnail"></a>--}}
-                        {{--<a class="waves-effect" href="#"><img src="/img/people/woman1-small.jpg" alt="Thumbnail"></a>--}}
-                        {{--<a class="waves-effect" href="#"><img src="/img/people/man2-small.jpg" alt="Thumbnail"></a>--}}
-                    {{--</div>--}}
-                    {{--<div class="instgr-row clearfix">--}}
-                        {{--<a class="waves-effect" href="#"><img src="/img/people/woman2-small.jpg" alt="Thumbnail"></a>--}}
-                        {{--<a class="waves-effect" href="#"><img src="/img/people/man3-small.jpg" alt="Thumbnail"></a>--}}
-                        {{--<a class="waves-effect" href="#"><img src="/img/people/woman3-small.jpg" alt="Thumbnail"></a>--}}
-                    {{--</div>--}}
-                    {{--<a href="#" class="text-smaller">Follow us on Instagram</a>--}}
-                {{--</div>--}}
+                <!-- Instagram Posts -->
+                <div class="offcanvas-instagram">
+                    <div class="instgr-row clearfix">
+                        <a class="waves-effect" href="#"><img src="/img/people/man1-small.jpg" alt="Thumbnail"></a>
+                        <a class="waves-effect" href="#"><img src="/img/people/woman1-small.jpg" alt="Thumbnail"></a>
+                        <a class="waves-effect" href="#"><img src="/img/people/man2-small.jpg" alt="Thumbnail"></a>
+                    </div>
+                    <div class="instgr-row clearfix">
+                        <a class="waves-effect" href="#"><img src="/img/people/woman2-small.jpg" alt="Thumbnail"></a>
+                        <a class="waves-effect" href="#"><img src="/img/people/man3-small.jpg" alt="Thumbnail"></a>
+                        <a class="waves-effect" href="#"><img src="/img/people/woman3-small.jpg" alt="Thumbnail"></a>
+                    </div>
+                    <a href="#" class="text-smaller">Follow us on Instagram</a>
+                </div>
 
 
 
