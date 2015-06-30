95bf7eac3dccdf8cf2c1cbbb61dbaa3ce2e197c1
Raynold van Heyningen <raynoldvanheyningen@gmail.com>
2015-06-30 18:11:57 +0200

    Added SOCIAL_LOGIN_REDIRECT_URL environment variable

--- config/services.php
+++ config/services.php
@@ -38,19 +38,19 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' => 'http://onesecretfantasy.dev/login/facebook',
+        'redirect' => env('SOCIAL_LOGIN_REDIRECT_URL'),
     ],
 
     'twitter' => [
         'client_id' => env('TWITTER_CLIENT_ID'),
         'client_secret' => env('TWITTER_CLIENT_SECRET'),
-        'redirect' => 'http://onesecretfantasy.dev/login/twitter',
+        'redirect' => env('SOCIAL_LOGIN_REDIRECT_URL'),
     ],
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://onesecretfantasy.dev/login/google',
+        'redirect' => env('SOCIAL_LOGIN_REDIRECT_URL'),
     ]
 
 ];
