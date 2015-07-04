d78c4876023d1c8ddab98de4db617b35caf5e2e4
Raynold van Heyningen <raynoldvanheyningen@gmail.com>
2015-07-04 23:27:21 +0200

    updated gettags function, added failsafe method when no splitter has been entered

--- app/Blog.php
+++ app/Blog.php
@@ -17,7 +17,8 @@ class Blog extends Model {
 
     public function getTags() {
         if (str_contains(';', $this->tags)) return explode(";", $this->tags);
-        if (str_contains(',', $this->tags)) return explode(",", $this->tags);
+        else if (str_contains(',', $this->tags)) return explode(",", $this->tags);
+        else return $this->tags;
     }
 
     public function getRelated() {
