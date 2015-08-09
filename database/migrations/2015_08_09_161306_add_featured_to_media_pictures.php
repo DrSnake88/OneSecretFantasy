75864d08f21ee4539e9e49c51243394de95f0b70
Raynold van Heyningen <raynoldvanheyningen@gmail.com>
2015-08-09 18:15:58 +0200

    add featured to pictures to display on home page

---
+++ database/migrations/2015_08_09_161306_add_featured_to_media_pictures.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFeaturedToMediaPictures extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('media_pictures', function(Blueprint $table)
+		{
+			$table->boolean('featured')->default(0);
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('media_pictures', function(Blueprint $table)
+		{
+			$table->removeColumn('featured');
+		});
+	}
+
+}
