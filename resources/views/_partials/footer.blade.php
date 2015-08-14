08472f70f0e62bf7c7b55894c8368d8ce30120de
Raynold van Heyningen <raynoldvanheyningen@gmail.com>
2015-08-14 21:14:17 +0200

    Undeferred JS file / Deferred CSS

--- resources/views/_partials/footer.blade.php
+++ resources/views/_partials/footer.blade.php
@@ -24,7 +24,7 @@
 {{--<script src="//oss.maxcdn.com/modernizr/2.8.3/modernizr.min.js"></script>--}}
 {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/detectizr/1.5.0/detectizr.min.js"></script>--}}
 {{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>--}}
-{{--<script src="{{ asset('/js/vendor.js') }}" async></script>--}}
+<script src="{{ asset('/js/vendor.js') }}" async></script>
 
 <script data-cfasync="false">
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -40,15 +40,27 @@
 <!-- Go to www.addthis.com/dashboard to customize your tools -->
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-55986a05432f7646#async=1" async="async"></script>
 
-<script type="text/javascript">
-    function downloadJSAtOnload() {
-        var element = document.createElement("script");
-        element.src = "js/vendor.js";
-        document.body.appendChild(element);
-    }
-    if (window.addEventListener)
-        window.addEventListener("load", downloadJSAtOnload, false);
-    else if (window.attachEvent)
-        window.attachEvent("onload", downloadJSAtOnload);
-    else window.onload = downloadJSAtOnload;
+{{--<script type="text/javascript">--}}
+    {{--function downloadJSAtOnload() {--}}
+        {{--var element = document.createElement("script");--}}
+        {{--element.src = "js/vendor.js";--}}
+        {{--document.body.appendChild(element);--}}
+    {{--}--}}
+    {{--if (window.addEventListener)--}}
+        {{--window.addEventListener("load", downloadJSAtOnload, false);--}}
+    {{--else if (window.attachEvent)--}}
+        {{--window.attachEvent("onload", downloadJSAtOnload);--}}
+    {{--else window.onload = downloadJSAtOnload;--}}
+{{--</script>--}}
+
+<script>
+    var cb = function() {
+        var l = document.createElement('link'); l.rel = 'stylesheet';
+        l.href = 'css/app.css';
+        var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
+    };
+    var raf = requestAnimationFrame || mozRequestAnimationFrame ||
+            webkitRequestAnimationFrame || msRequestAnimationFrame;
+    if (raf) raf(cb);
+    else window.addEventListener('load', cb);
 </script>
